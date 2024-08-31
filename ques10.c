c7fb2c0ef8ad131b8115978c2b7c97caa4792b37
PihooGokhle <pihoogokhle@gmail.com>
2024-09-01 00:50:48 +0530

    add ques10.c

---
+++ ques10.c
@@ -0,0 +1,19 @@
+# include<stdio.h>
+# include<math.h>
+
+int main(){
+    
+    int radius;
+    
+    printf("enter radius");
+    scanf("%d" , &radius);
+
+    int area = 3.14*radius*radius;
+
+    printf("%d" , area );
+
+    return 0;
+
+}
+
+
