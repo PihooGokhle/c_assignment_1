ec0b9f447dc53b9fb853386e03a11d801c9f9766
PihooGokhle <pihoogokhle@gmail.com>
2024-09-01 00:33:14 +0530

    add ques9.c file

---
+++ ques9.c
@@ -0,0 +1,19 @@
+# include<stdio.h>
+# include<math.h>
+
+int main(){
+
+    int a ,b;
+
+    printf("enter a");
+    scanf("%d" , &a);
+
+    printf("enter b");
+    scanf("%d" , &b);
+
+    int area = a*b;
+
+    printf("%d" , area);
+
+    return 0;
+}
